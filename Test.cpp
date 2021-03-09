44a543747f1cf86833af3b920302acf93f674898
uriyashemla <74508134+uriyashemla@users.noreply.github.com>
2021-03-09 07:42:25 -0500

    Add files via upload

---
+++ Test.cpp
@@ -0,0 +1,64 @@
+  
+#include "doctest.h"
+#include "snowman.hpp"
+#include <string>
+#include <algorithm>
+using namespace std;
+using namespace ariel;
+
+
+
+
+TEST_CASE("good snowman"){
+    CHECK(snowman(12114411) == "       \n _===_ \n (...) \n ( : ) \n ( : ) ");
+    CHECK(snowman(24114411) == "  ___  \n ..... \n (. .) \n ( : ) \n ( : ) ");
+    CHECK(snowman(31114411) == "   _   \n  /_\\  \n (.,.) \n ( : ) \n ( : ) ");
+    CHECK(snowman(43114411) == "  ___  \n (_*_) \n (._.) \n ( : ) \n ( : ) ");
+    CHECK(snowman(11114411) == "       \n  _===_ \n (.,.) \n ( : ) \n ( : ) ");
+}
+
+
+TEST_CASE("bigger than 8 digits"){
+    CHECK_THROWS(snowman(123412341));
+    CHECK_THROWS(snowman(314213412));
+    CHECK_THROWS(snowman(123412121));
+    CHECK_THROWS(snowman(421332142));
+    CHECK_THROWS(snowman(343421213));
+}
+
+TEST_CASE("lower than 8 digits"){
+    CHECK_THROWS(snowman(4));
+    CHECK_THROWS(snowman(21));
+    CHECK_THROWS(snowman(123));
+    CHECK_THROWS(snowman(4213));
+    CHECK_THROWS(snowman(21432));
+    CHECK_THROWS(snowman(212412));
+    CHECK_THROWS(snowman(2432114));
+}
+TEST_CASE("include digit that bigger than 4"){
+    CHECK_THROWS(snowman(51114411));
+    CHECK_THROWS(snowman(26123421));
+    CHECK_THROWS(snowman(32194444));
+    CHECK_THROWS(snowman(21228111));
+    CHECK_THROWS(snowman(22134833));
+    CHECK_THROWS(snowman(44444451));
+    CHECK_THROWS(snowman(11224416));
+}
+
+TEST_CASE("include digit that lower than 1"){
+    CHECK_THROWS(snowman(-51114411));
+    CHECK_THROWS(snowman(00000000));
+    CHECK_THROWS(snowman(12031211));
+    CHECK_THROWS(snowman(21223011));
+    CHECK_THROWS(snowman(22134830));
+}
+
+TEST_CASE("include note that is not digit"){
+    CHECK_THROWS(snowman(!1114411));
+    CHECK_THROWS(snowman(3/414313));
+    CHECK_THROWS(snowman(21+14221));
+    CHECK_THROWS(snowman(431-4332));
+    CHECK_THROWS(snowman(1241^411));
+    CHECK_THROWS(snowman(332241*4));  
+}
+
---
+++ snowman.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "snowman.hpp"
+#include <string>
+using namespace std;
+
+namespace ariel {
+   
+    string snowman(int a) {
+        return "true";
+    }
+}
